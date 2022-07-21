e8d17fd9b650d098e972dcd4025a282913871de1
joana <joanadarknes2233@gmail.com>
2022-07-21 15:47:37 -0300

    commit test

---
+++ Projeto-netflix/assets/js/temporadas.js
@@ -0,0 +1,27 @@
+let temporadas = [
+    {
+        id: 1,
+        nome: "Stranger Things",
+        sinopse: "Hawkins é a prova de que um verão pode mudar tudo: entre romances e o novo centro comercial, a cidade passa por grandes modificações na estação quente de 1985!",
+        imagemURL: "https://wallpapercave.com/wp/wp5621882.jpg",
+        imagemIcone: "https://wallpapercave.com/wp/wp5621882.jpg",
+        temporada: "Temporada 3"
+    },
+    {
+        id: 2,
+        nome: "Stranger Things",
+        sinopse: "Passou quase um ano desde o estranho desaparecimento de Will. Mas a vida em Hawkins não voltou à normalidade. Nem sombra disso.",
+        imagemURL: "https://imgix.bustle.com/inverse/aa/f6/8f/04/e342/40ca/a505/4b8c6a0c0770/promo325034471png.png?w=760&h=427&fit=max&auto=format%2Ccompress&q=50&dpr=2",
+        imagemIcone: "https://cdn.benchmark.pl/uploads/backend_img/c/recenzje/2021_02/10868_netflix-seriale-sf/stranger_things.jpg",
+        temporada: "Temporada 2"
+    },
+    {
+        id: 3,
+        nome: "Stranger Things",
+        sinopse: "Coisas estranhas acontecem em Hawkins, Indiana, onde o desaparecimento súbito de um rapaz revela uma jovem rapariga com poderes do outro mundo.",
+        imagemURL: "https://th.bing.com/th/id/R.882140a51695c485ec532467e5bca3b6?rik=gL1q%2bAgf1O2Ceg&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fwp1839583.jpg&ehk=UPYHoyfTMbiixXcInHD0D%2bSmsdABUCNbiuKikGZhFwc%3d&risl=&pid=ImgRaw&r=0",
+        imagemIcone:"https://th.bing.com/th/id/R.1d859c6946eae39fde8b593d2f5e0da0?rik=jwGcwuL8DpqtkA&riu=http%3a%2f%2fwww.vortexcultural.com.br%2fimages%2f2016%2f07%2fStranger-Things-destaque-Netflix.jpg&ehk=FgYdWp7AIuV%2fBmWdBx%2fsnKsRDdNtmfafnbd2%2f8zOacE%3d&risl=&pid=ImgRaw&r=0",
+        temporada: "Temporada 1"
+    },
+    
+]
